264b915653c1134942a2e9d3ab91dc820ad83f62
aru120 <khuang159@gmail.com>
2020-10-06 22:25:05 -0400

    added school class and methods

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,41 @@
+require 'pry'
+
 # code here!
+class School
+
+    
+    def initialize(school_name)
+        @school = school_name
+        @roster = {}
+    end
+
+    def roster
+        @roster 
+    end
+
+    def add_student(name,age)
+       
+        if @roster.has_key?(age)
+            @roster[age] << name
+        else
+        @roster[age] = []
+        @roster[age] << name
+        end     
+    end
+
+    def grade(age)
+       @roster[age]    
+    end
+
+    def sort
+        @sorted_hash = {}
+        @roster.each do |k, v|
+            @sorted_hash[k] = v.sort
+           
+        end
+        @sorted_hash
+       
+    end
+
+
+end
